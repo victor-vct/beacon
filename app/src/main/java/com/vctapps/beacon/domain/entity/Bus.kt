cf96c6dfd8a19c05119b6423a4e08e2e563d8682
Victor Vieira <victorvieira302@gmail.com>
2017-07-27 23:47:37 -0300

    added domain entity
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

---
+++ app/src/main/java/com/vctapps/beacon/domain/entity/Bus.kt
@@ -0,0 +1,10 @@
+package com.vctapps.beacon.domain.entity
+
+import java.util.*
+
+data class Bus(val id: Int,
+               val line: String,
+               val origin: String,
+               val destiny: String,
+               var isFavorite: Boolean,
+               val arrivalAt: Calendar)
